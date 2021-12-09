3be325a8ce493dabe6b06b700e0ae516b45a3fed
Harsh Trivedi <htrivedi@workfusion.com>
2021-12-08 19:54:13 -0500

    spring boot app unable to authenticate with postgres container - issue fixed add bean name in properties

--- src/main/java/com/nielsen/interview/carservicingappointmentsystem/config/JasyptConfig.java
+++ src/main/java/com/nielsen/interview/carservicingappointmentsystem/config/JasyptConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JasyptConfig {
 
-    @Bean
+    @Bean("encryptor")
     public StringEncryptor getPasswordEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
