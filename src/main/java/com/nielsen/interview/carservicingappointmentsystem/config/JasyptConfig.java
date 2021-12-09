86053078d44864a5177b7bc9b7e423887c5e5500
Harsh Trivedi <htrivedi@workfusion.com>
2021-12-08 19:34:10 -0500

    Constants in a separate class

--- src/main/java/com/nielsen/interview/carservicingappointmentsystem/config/JasyptConfig.java
+++ src/main/java/com/nielsen/interview/carservicingappointmentsystem/config/JasyptConfig.java
@@ -1,5 +1,6 @@
 package com.nielsen.interview.carservicingappointmentsystem.config;
 
+import com.nielsen.interview.carservicingappointmentsystem.constants.JasyptConstants;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
@@ -8,22 +9,19 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JasyptConfig {
+
     @Bean
     public StringEncryptor getPasswordEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-
-        config.setPassword("harsh");
-
-        config.setAlgorithm("PBEWithMD5AndDES");
-        config.setKeyObtentionIterations("1000");
-        config.setPoolSize("1");
-        config.setProviderName("SunJCE");
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-        config.setStringOutputType("base64");
-
+        config.setPassword(JasyptConstants.PASSWORD);
+        config.setAlgorithm(JasyptConstants.ALGORITHM);
+        config.setKeyObtentionIterations(JasyptConstants.KEY_OBTENTION_ITERATIONS);
+        config.setPoolSize(JasyptConstants.POOL_SIZE);
+        config.setProviderName(JasyptConstants.PROVIDER_NAME);
+        config.setSaltGeneratorClassName(JasyptConstants.JASYPT_SALT_RANDOM_SALT_GENERATOR);
+        config.setStringOutputType(JasyptConstants.OUTPUT_TYPE);
         encryptor.setConfig(config);
-
         return encryptor;
     }
 
---
+++ src/main/java/com/nielsen/interview/carservicingappointmentsystem/constants/JasyptConstants.java
@@ -0,0 +1,12 @@
+package com.nielsen.interview.carservicingappointmentsystem.constants;
+
+public class JasyptConstants {
+    public static final String PASSWORD = "harsh";
+    public static final String ALGORITHM = "PBEWithMD5AndDES";
+    public static final String KEY_OBTENTION_ITERATIONS = "1000";
+    public static final String POOL_SIZE = "1";
+    public static final String PROVIDER_NAME = "SunJCE";
+    public static final String JASYPT_SALT_RANDOM_SALT_GENERATOR = "org.jasypt.salt.RandomSaltGenerator";
+    public static final String OUTPUT_TYPE = "base64";
+}
+
